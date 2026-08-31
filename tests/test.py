2a9390b3cbb743e1d4a5d6da7e7b67cb81ac70ce
MementolanAngwitch <billouyang3210@gmail.com>
2026-08-31 13:35:09 -0700

    i don't want to commit, but there's an error somewhere

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 from physicalDrum.paths import OUTPUT, ensure_dirs
 import physicalDrum.drum as drum 
+#import physicalDrum.experimental as drum
 import matplotlib.pyplot as plt 
 from datetime import datetime
 import numpy as np
@@ -35,16 +36,20 @@ sr =48000
 
 # print("soft_strike and hard_strike wav files generated")
 
-y_s, *_ = drum.sequential_strike(table, props, P=4.25e-3, normalize=False)
-y_h, *_ = drum.sequential_strike(table, props, P=3.46e-2, normalize=False)
-peak = np.abs(y_h).max()
-# drum.write('modes_c_soft.wav', 0.95*y_s/peak, sr)
+# y_s, *_ = drum.sequential_strike(table, props, P=4.25e-3, normalize=False)
+# y_h, *_ = drum.sequential_strike(table, props, P=3.46e-2, normalize=False)
+# peak = np.abs(y_h).max()
+# # drum.write('modes_c_soft.wav', 0.95*y_s/peak, sr)
 # drum.write('modes_c_hard.wav', 0.95*y_h/peak, sr)
 
 # print("soft_strike and hard_strike wav files generated")
-drum.write(run / "soft.wav", 0.95 * y_s / peak, sr)
-drum.write(run / "hard.wav", 0.95 * y_h / peak, sr)
+# drum.write(run / "soft.wav", 0.95 * y_s / peak, sr)
+# drum.write(run / "hard.wav", 0.95 * y_h / peak, sr)
 
 # y_s, *_ = drum.sequential_strike(table, props, P=4.25e-3, normalize=False)
 # peak = np.abs(y_s).max()
 # drum.write('new_modes()_soft.wav', 0.95*y_s/peak, sr)
+
+for L in [1, 4, 8, 16, 32]:
+    y, S, gc, ga = drum.sequential_strike(table, props, P=3.46e-2, L_slow=L)
+    print(L, ga.max(), S.max())
